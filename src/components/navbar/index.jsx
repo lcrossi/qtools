70c406e99a62a0f5e48f254ef2f9fc41c89e9d9a
Luis Rossi <lcrossi2010@gmail.com>
2023-11-19 18:31:59 -0300

    Add navbar navigation Link

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -9,7 +9,9 @@ export default function Navbar() {
 
     return (
         <Stack direction='horizontal' className='navbar fluid'>
-            <Button variant="secondary" className='p-2 ms-3'>{"< Voltar"}</Button>
+            <Link to="/">
+                <Button variant="secondary" className='p-2 ms-3'>{"< Voltar"}</Button>
+            </Link>
             <Figure.Image
                 className='ms-auto fluid'
                 width={110}
