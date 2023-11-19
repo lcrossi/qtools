a8dc16301e09b9ff67da8aceb96427dfcd803960
Luis Rossi <lcrossi2010@gmail.com>
2023-11-19 18:23:23 -0300

    Setting up ishikawa form functionality

--- src/components/ishikawa/index.jsx
+++ src/components/ishikawa/index.jsx
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import { Button, Form, Row, Card, Col, Container, Dropdown } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Button, Form, Row, Card, Col, Container, Modal } from 'react-bootstrap'
 import Fishbone from '@hophiphip/react-fishbone';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './ishikawa.css'
@@ -12,8 +11,20 @@ export default function Ishikawa() {
     const [ishItems, setIshItems] = useState(0)
     const [ishPrincipal, setIshPrincipal] = useState('Prob principal')
     const [ishMetodo, setIshMetodo] = useState([])
+    const [ishMao, setIshMao] = useState([])
+    const [ishMaterial, setIshMaterial] = useState([])
+    const [ishMaquina, setIshMaquina] = useState([])
+    const [ishAmbiente, setIshAmbiente] = useState([])
+    const [ishMedicao, setIshMedicao] = useState([])
+    const [showModalProbAdded, setShowModalProbAdded] = useState(false);
+    const [showModalProbError, setShowModalProbError] = useState(false);
     const [fishboneChartMinWidth, setFishboneChartMinWidth] = useState(900)
-    
+    var metodoProblem = ''
+    var maoProblem = ''
+    var materialProblem = ''
+    var maquinaProblem = ''
+    var ambienteProblem = ''
+    var medicaoProblem = ''
     /* useEffect(() => {
          setIshItems ({
             "name": ishPrincipal,
@@ -62,9 +73,102 @@ export default function Ishikawa() {
         }
 
         if (type == 'metodo') {
-            setIshMetodo(ishMetodo.push({"name": value}))
-            console.log("metodo changed", ishMetodo)
+            metodoProblem = value
         }
+
+        if (type == 'mao') {
+            maoProblem = value
+        }
+
+        if (type == 'material') {
+            materialProblem = value
+        }
+
+        if (type == 'maquina') {
+            maquinaProblem = value
+        }
+
+        if (type == 'ambiente') {
+            ambienteProblem = value
+        }
+
+        if (type == 'medicao') {
+            medicaoProblem = value
+        }
+    }
+
+    function addIshProblem(type) {
+        if (type == 'metodo') {
+            if (metodoProblem && !metodoProblem.match(/^(\s)+$/)) { /* Essa expressão regular vai testar esse texto procurando garantir que a no inicio do texto (^) existe espaço em branco (\s) na quantidade de 1 ou mais (+) e que depois disso o texto termina ($). E depois nego o match com ! no inicio dessa condição, ou seja para garantid que esse if só dá true se não houver match. */
+                setIshMetodo([...ishMetodo, {"name": metodoProblem}])
+                handleShowModalProbs('added')
+            } else {
+                console.log('Dados invalidos')
+                handleShowModalProbs('error')
+            }
+        }
+
+        if (type == 'mao') {
+            if (type == 'mao') {
+                if (maoProblem && !maoProblem.match(/^(\s)+$/)) {
+                    setIshMao([...ishMao, {"name": maoProblem}])
+                    handleShowModalProbs('added')
+                } else {
+                    console.log('Dados invalidos')
+                    handleShowModalProbs('error')                    
+                }
+            }
+        }
+
+        if (type == 'material') {
+            if (type == 'material') {
+                if (materialProblem && !materialProblem.match(/^(\s)+$/)) {
+                    setIshMaterial([...ishMaterial, {"name": materialProblem}])
+                    handleShowModalProbs('added')
+                } else {
+                    console.log('Dados invalidos')
+                    handleShowModalProbs('error')                    
+                }
+            }
+        }
+        
+        if (type == 'maquina') {
+            if (type == 'maquina') {
+                if (maquinaProblem && !maquinaProblem.match(/^(\s)+$/)) {
+                    setIshMaquina([...ishMaquina, {"name": maquinaProblem}])
+                    handleShowModalProbs('added')
+                } else {
+                    console.log('Dados invalidos')
+                    handleShowModalProbs('error')                    
+                }
+            }
+        }
+
+        if (type == 'ambiente') {
+            if (type == 'ambiente') {
+                if (ambienteProblem && !ambienteProblem.match(/^(\s)+$/)) {
+                    setIshAmbiente([...ishAmbiente, {"name": ambienteProblem}])
+                    handleShowModalProbs('added')
+                } else {
+                    console.log('Dados invalidos')
+                    handleShowModalProbs('error')                    
+                }
+            }
+        }
+
+        if (type == 'medicao') {
+            if (type == 'medicao') {
+                if (medicaoProblem && !medicaoProblem.match(/^(\s)+$/)) {
+                    setIshMedicao([...ishMedicao, {"name": medicaoProblem}])
+                    handleShowModalProbs('added')
+                } else {
+                    console.log('Dados invalidos')
+                    handleShowModalProbs('error')                    
+                }
+            }
+        }
+        //limpando o input
+        document.getElementById(`input_${type}`).value = ''
     }
 
     function loadIshikawa() {
@@ -80,52 +184,23 @@ export default function Ishikawa() {
                     },
                     {
                         "name": "Mão de Obra",
-                        "children": [
-
-                            {"name": "Shifts"},
-                            {"name": "Training"},
-                            {"name": "Operators"}
-                        ]
+                        "children": ishMao,
                     },
                     {
                         "name": "Material",
-                        "children": [
-                            {"name": "Brake"},
-                            {"name": "Angle Angle Angle AngleAngle AngleAngle Angle AngleAngle"},
-                        ]
+                        "children": ishMaterial
                     },
                     {
                         "name": "Máquina",
-                        "children": [
-                            {
-                                "name": "Quality",
-                                "children": [
-                                    {"name": "Delivery"},
-                                ]
-                            },
-                        ]
+                        "children": ishMaquina
                     },
                     {
                         "name": "Meio Ambiente",
-                        "children": [
-                            {
-                                "name": "Quality",
-                                "children": [
-                                    {"name": "Delivery"},
-                                ]
-                            },
-                        ]
+                        "children": ishAmbiente
                     },
                     {
                         "name": "Meio de Medição",
-                        "children": [
-                            {
-                                "name": "Quality",
-                                "children": [
-                                    {"name": "Delivery"},
-                                ]
-                            },
-                        ]
+                        "children": ishMedicao
                     },
                 ]
             })
@@ -135,6 +210,28 @@ export default function Ishikawa() {
             setShowOrHide('show')
             setBtnLabel('Salvar')
             setRenderIshChart(false)
+        }
+    }
+
+    function handleShowModalProbs(type) {
+        if (type == "added") {
+            setShowModalProbAdded(true)
+            console.log(showModalProbAdded)
+            
+            setTimeout(() => {
+                setShowModalProbAdded(false)
+            }, 
+            1500);
+        }
+
+        if (type == "error") {
+            setShowModalProbError(true)
+            console.log(showModalProbError)
+            
+            setTimeout(() => {
+                setShowModalProbError(false)
+            }, 
+            1500);
         }
     }
 
@@ -175,12 +272,22 @@ export default function Ishikawa() {
 
     return (
         <>
+            <Modal size="sm" show={showModalProbAdded} onHide={() => setShowModalProbAdded(false)}>
+                <Modal.Header closeButton>
+                <Modal.Title>Problema adicionado</Modal.Title>
+                </Modal.Header>
+            </Modal>
+            <Modal size="sm" show={showModalProbError} onHide={() => setShowModalProbError(false)}>
+                <Modal.Header closeButton>
+                <Modal.Title>Erro ao adicionar!</Modal.Title>
+                </Modal.Header>
+            </Modal>
             <Card id="ishiForm" className="p-3 mb-4">
                 <div className={`ishFormHeader`}>
                     <h2 className='mb-4'>Diagrama de Ishikawa</h2>
                 </div>
                 <Form className={showOrHide}>
-                    <Form.Group className="mb-5" controlId="exampleForm.ControlInput1">
+                    <Form.Group className="mb-5">
                         <Form.Label>Problema inicial</Form.Label>
                         <Form.Control 
                             type="text" 
@@ -188,40 +295,53 @@ export default function Ishikawa() {
                             onChange={(e) => handleInputChange('problemaPrincipal', e.target.value)}
                         />
                     </Form.Group>
-                    <Form.Group className="mb-5" controlId="exampleForm.ControlInput1">
+                    <Form.Group className="mb-5">
                         <Form.Label>Problema - Método</Form.Label>
-                        <Form.Control className='mb-2' type="text" placeholder="Adicione um problema por vez" />
-                        <Button variant='primary' className='mb-2'>Adicionar</Button>
+                        <Form.Control id='input_metodo' className='mb-2' type="text" 
+                            onChange={e => handleInputChange('metodo', e.target.value)}
+                            placeholder="Adicione um problema por vez" 
+                        />
+                        <Button onClick={() => addIshProblem('metodo')} variant='primary' className='mb-2'>Adicionar</Button>
                     </Form.Group>
-                    <Form.Group className="mb-5" controlId="exampleForm.ControlInput1">
+                    <Form.Group className="mb-5">
                         <Form.Label>Problema - Mão de Obra</Form.Label>
-                        <Form.Control className='mb-2' type="text" placeholder="Adicione um problema por vez" />
-                        <Button variant='primary' className='mb-2'>Adicionar</Button>
+                        <Form.Control id='input_mao' className='mb-2' type="text" 
+                            onChange={e => handleInputChange('mao', e.target.value)}
+                            placeholder="Adicione um problema por vez" 
+                        />
+                        <Button onClick={() => addIshProblem('mao')} variant='primary' className='mb-2'>Adicionar</Button>
                     </Form.Group>
-                    <Form.Group className="mb-5" controlId="exampleForm.ControlInput1">
+                    <Form.Group className="mb-5">
                         <Form.Label>Problema - Material</Form.Label>
-                        <Form.Control className='mb-2' type="text" placeholder="Adicione um problema por vez" />
-                        <Button variant='primary' className='mb-2'>Adicionar</Button>
+                        <Form.Control id='input_material' className='mb-2' type="text" 
+                            onChange={e => handleInputChange('material', e.target.value)}
+                            placeholder="Adicione um problema por vez" 
+                        />
+                        <Button onClick={() => addIshProblem('material')} variant='primary' className='mb-2'>Adicionar</Button>
                     </Form.Group>
-                    <Form.Group className="mb-5" controlId="exampleForm.ControlInput1">
+                    <Form.Group className="mb-5">
                         <Form.Label>Problema - Máquina</Form.Label>
-                        <Form.Control className='mb-2' type="text" placeholder="Adicione um problema por vez" />
-                        <Button variant='primary' className='mb-2'>Adicionar</Button>
+                        <Form.Control id='input_maquina' className='mb-2' type="text" 
+                            onChange={e => handleInputChange('maquina', e.target.value)}
+                            placeholder="Adicione um problema por vez" 
+                        />
+                        <Button onClick={() => addIshProblem('maquina')} variant='primary' className='mb-2'>Adicionar</Button>
                     </Form.Group>
-                    <Form.Group className="mb-5" controlId="exampleForm.ControlInput1">
-                        <Form.Label>Problema - Meio ambiente</Form.Label>
-                        <Form.Control className='mb-2' type="text" placeholder="Adicione um problema por vez" />
-                        <Button variant='primary' className='mb-2'>Adicionar</Button>
+                    <Form.Group className="mb-5">
+                        <Form.Label>Problema - Meio Ambiente</Form.Label>
+                        <Form.Control id='input_ambiente' className='mb-2' type="text" 
+                            onChange={e => handleInputChange('ambiente', e.target.value)}
+                            placeholder="Adicione um problema por vez" 
+                        />
+                        <Button onClick={() => addIshProblem('ambiente')} variant='primary' className='mb-2'>Adicionar</Button>
                     </Form.Group>
-                    <Form.Group className="mb-5" controlId="exampleForm.ControlInput1">
+                    <Form.Group className="mb-5">
                         <Form.Label>Problema - Meio de Medição</Form.Label>
-                        <Form.Control className='mb-2' type="text" placeholder="Adicione um problema por vez" />
-                        <Button variant='primary' className='mb-2'>Adicionar</Button>
-                    </Form.Group>
-                    <Form.Group className="mb-5" controlId="exampleForm.ControlInput1">
-                        <Form.Label>Problema - Gerenciamento</Form.Label>
-                        <Form.Control className='mb-2' type="text" placeholder="Adicione um problema por vez" />
-                        <Button variant='primary' className='mb-2'>Adicionar</Button>
+                        <Form.Control id='input_medicao' className='mb-2' type="text" 
+                            onChange={e => handleInputChange('medicao', e.target.value)}
+                            placeholder="Adicione um problema por vez" 
+                        />
+                        <Button onClick={() => addIshProblem('medicao')} variant='primary' className='mb-2'>Adicionar</Button>
                     </Form.Group>
                 </Form>
                 <Container className='fluid'>
