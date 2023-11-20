6a1a933973376509830ed6be1b1dd2c9075dd261
Luis Rossi <lcrossi2010@gmail.com>
2023-11-20 10:33:17 -0300

    inicializando componente 5pqs

---
+++ src/components/5pqs/index.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react'
+import { Button, Form, Row, Card, Col, Container, Modal } from 'react-bootstrap'
+
+import './5pqs.css'
+
+export default function Pqs() {
+    return (
+        <>
+        <Card>
+            5PQs aquis
+        </Card>
+        </>
+    )
+}
--- src/pages/tools/index.jsx
+++ src/pages/tools/index.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container, Stack } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from '../../components/navbar'
 import Ishikawa from '../../components/ishikawa'
+import Pqs from '../../components/5pqs'
 import './tools.css'
 
 
 export default function Tools() {
+    const [showHidePqs, setShowHidePqs] = useState('show')
+
     return(
         <>
         <Navbar/>
         <div className='toolsContainer'>
             <Stack >
-                <div className="ms-3"><Ishikawa/></div> 
+                <div className="ms-3"><Ishikawa /></div> 
+                <div className={`ms-3 ${showHidePqs}`}><Pqs /></div> 
             </Stack>
         </div>
         </>
